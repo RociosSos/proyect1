6a8567545ebe06796bce6b50126e3b5cb6f6bd14
Rocio <rociobelensosa1996@gmail.com>
2025-05-13 02:12:28 -0300

    cambios16

--- js/alta.js
+++ js/alta.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu__container"); // Asegúrate de que sea el contenedor correcto
+    const menu = document.querySelector(".menu__container");
 
     menuToggle.addEventListener("click", function() {
-        menu.classList.toggle("show"); // Añadir o quitar la clase 'show' para mostrar/ocultar el menú
+        menu.classList.toggle("show"); 
     });
 
-    // Cerrar el menú si se hace clic fuera de él
     document.addEventListener("click", function(event) {
         if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-            menu.classList.remove("show"); // Eliminar la clase 'show' para ocultar el menú
+            menu.classList.remove("show");
         }
     });
 });
